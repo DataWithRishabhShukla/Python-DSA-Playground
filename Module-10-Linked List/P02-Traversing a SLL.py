8b234d3d411d095601e1a36d336f08d322359d1f
rishabhshukla <rishabh.shukla8@gmail.com>
2024-07-03 16:47:13 +0530

    Added more information to the linked list module

---
+++ Module-10-Linked List/P02-Traversing a SLL.py
@@ -0,0 +1,18 @@
+
+class Node:
+    def __init__(self,k):
+        self.key = k
+        self.next = None
+
+def print_list(head):
+    curr = head
+    while curr != None:
+        print(curr.key, end = " ")
+        curr = curr.next
+# Shorter form of the code 
+
+head = Node(10)
+head.next = Node(20)
+head.next.next = Node(30)
+
+print_list(head)
