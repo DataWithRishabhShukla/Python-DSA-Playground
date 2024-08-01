a7972f7d31932059cc1d535b93c41b3e1255078d
rishabhshukla <rishabh.shukla8@gmail.com>
2024-08-01 08:22:36 +0530

    started on deque module

---
+++ Module-14-Deque/P01-Deque Implementation.py
@@ -0,0 +1,26 @@
+from collections import deque
+print("\nProgram 1!!!\n")
+d = deque()
+print(d)
+
+d.append(10)
+print(d)
+
+d.append(20)
+d.append(30)
+print(d)
+
+d.appendleft(40)
+print(d)
+
+print(d.pop())
+print(d.popleft())
+print(d)
+print("\n!!!End of Program 1!!!\n")
+
+from collections import deque
+print("\nProgram 1!!!\n")
+d = deque([10,20,30,40])
+print(d)
+
+print("\n!!!End of Program 1!!!\n")
