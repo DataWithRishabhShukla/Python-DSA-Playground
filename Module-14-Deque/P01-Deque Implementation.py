8ae0cbb356c95ae01793ba32b96fa42ba032cc0d
rishabhshukla <rishabh.shukla8@gmail.com>
2024-08-01 08:32:19 +0530

    complete first deque program

--- Module-14-Deque/P01-Deque Implementation.py
+++ Module-14-Deque/P01-Deque Implementation.py
@@ -16,11 +16,46 @@ print(d)
 print(d.pop())
 print(d.popleft())
 print(d)
-print("\n!!!End of Program 1!!!\n")
+print("\n!!!End of Program !!!\n")
 
 from collections import deque
-print("\nProgram 1!!!\n")
+print("\n!!Start ofProgram 2!!!\n")
 d = deque([10,20,30,40])
 print(d)
 
-print("\n!!!End of Program 1!!!\n")
+d.insert(2,10)
+print(d)
+d.remove(10)
+print(d)
+d.extend([50,60])
+print(d)
+d.extendleft([10,15]) # deque([15, 10, 20, 10, 30, 40, 50, 60])
+print(d)
+print("\n!!!End of Program 2!!!\n")
+
+
+print("\n!!Start ofProgram 3!!!\n")
+from collections import deque
+d = deque([10,20,30,40])
+print(d)
+d.rotate(2)
+print(d)
+d.rotate(-2)
+print(d)
+d.reverse()
+print(d)
+
+print("\n!!!End of Program 3!!!\n")
+
+
+print("\n!!Start ofProgram 4!!!\n")
+from collections import deque
+d = deque([10,20,30,40])
+print(d)
+print(d[2])
+d[2] = 100
+print(d)
+print(d[-1])
+print(d[0])
+
+print("\n!!!End of Program 4!!!\n")
