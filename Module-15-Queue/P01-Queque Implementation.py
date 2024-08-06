bc2779a7ad4958222c5fa78fb8c92933fea01721
rishabhshukla <rishabh.shukla8@gmail.com>
2024-08-06 08:02:47 +0530

    Implemented the queue

--- Module-15-Queue/P01-Queque Implementation.py
+++ Module-15-Queue/P01-Queque Implementation.py
@@ -1,61 +1,28 @@
+print("\n!!Start ofProgram 1 - Queue Using the List!!!\n")
+
+q = []
+q.append(10) #enque
+q.append(20)
+q.append(30)
+print(q)
+print(q.pop(0)) #deque
+print(q.pop(0)) #deque
+print(len(q)) #size
+print(q[0]) #getFront
+print(q[-1]) #getRear
+print("\n!!!End of Program 1!!!\n")
+
+
+print("\n!!Start ofProgram 2 - Queue Using the List!!!\n")
 from collections import deque
-print("\nProgram 1!!!\n")
-d = deque()
-print(d)
-
-d.append(10)
-print(d)
-
-d.append(20)
-d.append(30)
-print(d)
-
-d.appendleft(40)
-print(d)
-
-print(d.pop())
-print(d.popleft())
-print(d)
-print("\n!!!End of Program !!!\n")
-
-from collections import deque
-print("\n!!Start ofProgram 2!!!\n")
-d = deque([10,20,30,40])
-print(d)
-
-d.insert(2,10)
-print(d)
-d.remove(10)
-print(d)
-d.extend([50,60])
-print(d)
-d.extendleft([10,15]) # deque([15, 10, 20, 10, 30, 40, 50, 60])
-print(d)
+q = deque()
+q.append(10) #enque
+q.append(20)
+q.append(30)
+print(q)
+print(q.popleft()) #deque
+print(q.popleft()) #deque
+print(len(q)) #size
+print(q[0]) #getFront
+print(q[-1]) #getRear
 print("\n!!!End of Program 2!!!\n")
-
-
-print("\n!!Start ofProgram 3!!!\n")
-from collections import deque
-d = deque([10,20,30,40])
-print(d)
-d.rotate(2)
-print(d)
-d.rotate(-2)
-print(d)
-d.reverse()
-print(d)
-
-print("\n!!!End of Program 3!!!\n")
-
-
-print("\n!!Start ofProgram 4!!!\n")
-from collections import deque
-d = deque([10,20,30,40])
-print(d)
-print(d[2])
-d[2] = 100
-print(d)
-print(d[-1])
-print(d[0])
-
-print("\n!!!End of Program 4!!!\n")
--- Module-15-Queue/Queque Notes.md
+++ Module-15-Queue/Queque Notes.md
@@ -22,5 +22,44 @@
 #### Video 2 - Queue in Python 
 - Using List 
 - Using Collections.deque
-- Using queue.Queue
+- Using queue.Queue 
+    - Only used in multithreded environmenets 
+    - Uses Collections.deque internally 
 - Our own Implementation 
+
+- **Using List**
+    - Only disadvantage in pop uses O(n) time as it removes and shift all elements.
+    - enque -> O(1)
+    - deque -> O(n)
+```python
+print("\n!!Start ofProgram 1 - Queue Using the List!!!\n")
+
+q = []
+q.append(10) #enque
+q.append(20)
+q.append(30)
+print(q)
+print(q.pop(0)) #deque
+print(q.pop(0)) #deque
+print(len(q)) #size
+print(q[0]) #getFront
+print(q[-1]) #getRear
+print("\n!!!End of Program 1!!!\n")
+```
+
+- **Using collection deque**
+    - Recomended way to implement queue in python
+    - Internally uses python 
+```python
+from collections import deque
+q = deque()
+q.append(10) #enque
+q.append(20)
+q.append(30)
+print(q)
+print(q.popleft()) #deque
+print(q.popleft()) #deque
+print(len(q)) #size
+print(q[0]) #getFront
+print(q[-1]) #getRear
+```
